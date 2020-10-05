2d341f8d64f087afd4f5a8e57f525c14d0fb5d5b
Dominik Mikolajczyk <jokerekv2@gmail.com>
2020-10-05 19:46:18 +0200

    Poprawienie kodu

--- src/MovieLibrary.java
+++ src/MovieLibrary.java
@@ -11,7 +11,7 @@ class MovieLibrary {
         System.out.printf("Tytuł: %s\n", movie.title);
         System.out.printf("Opis: %s\n", movie.description);
         System.out.printf("Reżyser: %s\n", movie.director);
-        System.out.printf("Ocena: %f/10\n", movie.rating);
+        System.out.printf("Ocena: %d/10\n", movie.rating);
         System.out.printf("Czy otrzymał Oskara: %s\n", movie.oscars);
     }
 }
